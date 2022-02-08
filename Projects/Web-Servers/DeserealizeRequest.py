125b1222d3aee1eb67643742079cd3bdf4fdf26c
CSI-Ezequiel-Ramirez <24-107@sanignacio.pr>
2022-02-08 10:19:58 -0400

    Deserealize Request WIP

--- Projects/Web-Servers/DeserealizeRequest.py
+++ Projects/Web-Servers/DeserealizeRequest.py
@@ -1,6 +1,8 @@
 import json, ssl
+from pathlib import Path
 import urllib.request
 from RandomBeer import RandomBeer
+import os
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -8,10 +10,32 @@ BeerURL = "https://random-data-api.com/api/beer/random_beer?size=100"
 
 beers:RandomBeer = []
 
+# create directory
+myPath = Path(__file__).parents[0]
+myFilePath = os.path.join(myPath, 'responses')
+
+if not os.path.exists("responses"):
+    os.mkdir(myFilePath)
+    print("Directory " , "responses" , " created.")
+else:
+    print("Directory ", "responses" , " created.")
+
+
+
+# loop 100 times
+i = 1
+
 Req = urllib.request.Request(BeerURL)
 RequestData = json.loads(urllib.request.urlopen(Req).read())
 
-for r in RequestData:
-    beer:RandomBeer = RandomBeer(**r)
-    beers.append(beer)
-    print(beer.name)
+while i <= 100:
+    i + 1
+    Req = urllib.request.Request(BeerURL)
+    RequestData = json.loads(urllib.request.urlopen(Req).read())
+
+    for r in RequestData:
+        beer:RandomBeer = RandomBeer(**r)
+        beers.append(beer)
+        print(beer.name)
+
+
