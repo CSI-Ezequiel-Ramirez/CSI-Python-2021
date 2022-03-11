e736a1e7c1477396fa9e6db9731219fc7f83d05d
CSI-Ezequiel-Ramirez <24-107@sanignacio.pr>
2022-03-11 16:18:36 -0400

    Snake 3

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -6,7 +6,8 @@ pygame.display.set_caption('Snake game by ya boi Zeke') #Credits the one and onl
 game_over=False #Makes the default Game Over state as false, allowing you to play the game.
 while not game_over: #Begins a while loop while the Game Over state is false.
     for event in pygame.event.get(): #Gets all of the actions that take place on the screen.
-        print(event)   #Prints out all the actions that take place on the screen.
+        if event.type==pygame.QUIT: #Makes it so that if the quit button is pressed on the UI, then the game quits.
+            game_over=True #Makes the Game Over state true.
 
 pygame.quit() #Quits the game. Pretty self-explanatory.
 quit() #Quits the Python script. Even MORE self-explanatory.
