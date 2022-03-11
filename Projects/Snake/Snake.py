421fd758da623424dbb0dd1b9dbf3954a030b8c2
CSI-Ezequiel-Ramirez <24-107@sanignacio.pr>
2022-03-11 15:30:56 -0400

    Snake 1

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,6 @@
+import pygame #Imports the Pygame package, which is crucial for pretty much everything here.
+pygame.init() #Initializes the Pygame module, which then allows everything that comes after to function.
+dis=pygame.display.set_mode((400,300)) #Creates the screen that users will use to view the game.
+pygame.display.update() #This method updates any changes made to said screen.
+pygame.quit() #Quits the game. Pretty self-explanatory.
+quit() #Quits the Python script. Even MORE self-explanatory.
