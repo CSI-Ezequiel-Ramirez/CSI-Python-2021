156afdf40cbfb2212d95d52033699dd969905bc8
CSI-Ezequiel-Ramirez <24-107@sanignacio.pr>
2022-03-11 16:15:51 -0400

    Snake 2

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -2,5 +2,11 @@ import pygame #Imports the Pygame package, which is crucial for pretty much ever
 pygame.init() #Initializes the Pygame module, which then allows everything that comes after to function.
 dis=pygame.display.set_mode((400,300)) #Creates the screen that users will use to view the game.
 pygame.display.update() #This method updates any changes made to said screen.
+pygame.display.set_caption('Snake game by ya boi Zeke') #Credits the one and only creator of this script in the top of the screen.
+game_over=False #Makes the default Game Over state as false, allowing you to play the game.
+while not game_over: #Begins a while loop while the Game Over state is false.
+    for event in pygame.event.get(): #Gets all of the actions that take place on the screen.
+        print(event)   #Prints out all the actions that take place on the screen.
+
 pygame.quit() #Quits the game. Pretty self-explanatory.
 quit() #Quits the Python script. Even MORE self-explanatory.
